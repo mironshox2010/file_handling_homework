c4f8f39b48a561f3d95d105f178c75aa1e4b2742
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-04-18 22:43:03 +0500

    file04.py

--- file04.py
+++ file04.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    ans = []
+    for num in data:
+        if not num.isdigit():
+            ans.append(num)
+    return ans    
 # Read data from file
+f = open('txt_file/data04.txt', encoding='UTF-8')
+data = f.read()
+print(main(data))
