62c6a3156e0ace375eff392a8df54a7cc959c6f0
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-04-19 18:40:21 +0500

    file02.py

--- file02.py
+++ file02.py
@@ -1,3 +1,4 @@
+
 def main(data:str):
     """
     The data is from the file. Return number of characters in the file.
@@ -6,12 +7,7 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    lst = data.split(',')
-    ans = 0
-    for i in lst:
-        ans += len(i)
-    return ans    
-# Read data from file
-f = open('txt_file/data02.txt', encoding='UTF-8')
-data = f.read()
+ 
+    return len(data)
+data = open("txt_file/data02.txt").read()
 print(main(data))
