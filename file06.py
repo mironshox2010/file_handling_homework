0c470d7ff39623d6ac51db187d2f9256202d22fb
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-04-26 16:08:58 +0500

    file06.py

--- file06.py
+++ file06.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    k = []
+    h = data.split()
+    for i in h:
+        k.append(len(i))
+        
+    return k
     
+data = open('txt_file/data06.txt').read()
+print(main(data))
+
 # Read data from file
